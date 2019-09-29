fe7aa1f44dc14de857103dfe2a0333d144197489
VK <vitya39@gmail.com>
2019-09-29 19:14:31 +0300

    Upd basic project structure

---
+++ src/main/java/org/healthdemo/controller/AppController.java
@@ -0,0 +1,17 @@
+package org.healthdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/hello/{name}")
+    public String getHelloPage(@PathVariable("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
